f1e51430c86f025a7cd0a016fa323539b0e59a2e
jasn4real <tobeobi@pursuit.org>
2022-08-03 20:34:31 -0400

    Tobe-Higher-Order-Array

--- src/03-filter.js
+++ src/03-filter.js
@@ -13,21 +13,35 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsBySaib(songs) {}
+function getSongsBySaib(songs) {
+  return songs.filter((song) => song.artist === "Saib");
+}
 
 /**
  * Returns an array of all songs where the runtime is over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getSongsOverThreeMinutes(songs) {}
+function getSongsOverThreeMinutes(songs) {
+  const songsOverThreeMins = songs.filter(song => (song.runtimeInSeconds > 180)) 
+  return songsOverThreeMins;
+}
 
 /**
  * Returns an array of songs where the song title is the same as the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]} An array of objects.
  */
-function getTitleTracks(songs) {}
+function getTitleTracks(songs) {
+  // const titleTracks = songs.filter((song) => {
+  //   if(song.title === song.album) {
+  //     return song.title
+  //   }
+  // })
+  // return titleTracks;
+  const titleTracks = songs.filter(song => (song.title === song.album)) 
+  return titleTracks;
+}
 
 module.exports = {
   getSongsBySaib,
--- src/06-sort.js
+++ src/06-sort.js
@@ -13,7 +13,9 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
+function sortByRuntimeAscending(songs) {
+  return songs.sort((a, b) => a.runtimeInSeconds - b.runtimeInSeconds)
+}
 
 /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
@@ -23,7 +25,23 @@ function sortByRuntimeAscending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+function sortByArtistNameDescending(songs) {
+  return songs.sort((a, b) => {
+    const artistA = a.artist.toUpperCase(); 
+    const artistB = b.artist.toUpperCase();
+    return artistB.localeCompare(artistA);
+  // return songs.sort(function (a, b) {
+  //   const artistA = a.artist.toUpperCase(); 
+  //   const artistB = b.artist.toUpperCase(); 
+  //   if (artistA < artistB) {
+  //     return 1;
+  //   }
+  //   if (artistA > artistB) {
+  //     return -1;
+  //   }
+  //   return 0;
+  }) 
+}
 
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
@@ -33,7 +51,23 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+function sortBySongTitleAscending(songs) {
+  return songs.sort((a, b) => {
+    const titleA = a.title.toUpperCase(); 
+    const titleB = b.title.toUpperCase();
+    return titleA.localeCompare(titleB);
+  // return songs.sort(function (a, b) {
+  //   const artistA = a.artist.toUpperCase(); 
+  //   const artistB = b.artist.toUpperCase(); 
+  //   if (artistA < artistB) {
+  //     return 1;
+  //   }
+  //   if (artistA > artistB) {
+  //     return -1;
+  //   }
+  //   return 0;
+  }) 
+}
 
 module.exports = {
   sortByRuntimeAscending,
